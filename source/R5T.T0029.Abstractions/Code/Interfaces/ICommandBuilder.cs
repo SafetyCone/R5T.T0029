87976392dc792df8c010ce31ccbb7cc7c41d4235
David Coats <david@getsafetycone.com>
2021-06-02 10:50:01 -0700

    Add R5T.T0029.Abstractions library. Add R5T.T0028 (ICommandBuilder) project-reference dependency. Add ICommandBuilder definition interface and implementation class.

---
+++ source/R5T.T0029.Abstractions/Code/Interfaces/ICommandBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+using R5T.T0028;
+
+
+namespace R5T.T0029
+{
+    public interface ICommandBuilder : IStringBuilder
+    {
+    }
+}
---
+++ source/R5T.T0029/Code/Classes/CommandBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using R5T.T0028;
+
+
+namespace R5T.T0029
+{
+    public class CommandBuilder : StringBuilder, ICommandBuilder
+    {
+        #region Static
+
+        public static new CommandBuilder New()
+        {
+            return new CommandBuilder();
+        }
+
+        #endregion
+    }
+}
